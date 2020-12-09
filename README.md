9f780891664765dd7867c7d2d26066bb6fd4d444
Poja Shad <pojashad@gmail.com>
2020-12-09 23:48:50 +0100

    version 2

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
 # OpenCV-LaneKeeping
 My attempt on Lane Keeping with OpenCV in C++. Wish me good luck!
 
+Thanks Jan Haložan, i followed this blog post for v0.2 @ https://janhalozan.com/2019/06/01/lane-detector/
+
 ### Status
-- [x] Region Of Interest (ROI)
+- [x] Region Of Interest (ROI) 
 - [x] ROI To Grayscale
 - [x] ROI To Canny (Edge Detection)
 - [x] ROI Gaussian Blur
 - [x] ROI Back to BGR
 - [x] Merge ROI back to source frame
-- [ ] Draw Lanes
-- [ ] Remove ROI from source frame and just keep the lanes
+- [x] Draw Lanes
+- [x] Remove ROI from source frame and just keep the lanes
 - [ ] Feedback from the program to control the vehicle between the lanes 
 
 
 ## Screenshots
+v 0.1
 ![alt text](./screenshots/screenshot1.png)
+v 0.2
+![alt text](./screenshots/screenshot2.png)
--- main.cpp
+++ main.cpp
@@ -3,71 +3,196 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/calib3d.hpp>
 
 using namespace cv;
 using namespace std;
-Mat roi(Mat src, Rect rect){
-    Mat roi;
-    rectangle(src, rect, Scalar::all(255), 2); // bara för att se vilken yta jag jobbar med
-    roi = src(rect);
-    return roi;
-}
-Mat convertToBGR(Mat processedFrame) {
-    Mat frameInBGR;
-    cvtColor(processedFrame, frameInBGR , COLOR_GRAY2BGR);
-    return frameInBGR;
-};
 
-Mat processFrame(Mat frame){
-    Mat grayscale, gaussianBlur, imageCanny, processedFrame;
-    cvtColor(frame, grayscale, COLOR_BGR2GRAY);
-    Canny(grayscale, imageCanny, 100, 300);
-    GaussianBlur(imageCanny, gaussianBlur, Size(5, 5), 0);
-    vector<Vec4i> lines;
-    HoughLinesP(gaussianBlur, lines, 1, CV_PI/180, 50, 10, 0);
-    for( size_t i = 0; i < lines.size(); i++ )
+
+vector<Point2f> slidingWindow(Mat image, Rect window)
+{
+    vector<Point2f> points;
+    const Size imgSize = image.size();
+    bool shouldBreak = false;
+
+    while (true)
     {
-        Vec4i l = lines[i];
-        line( gaussianBlur, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(255,255,255), 1, LINE_AA);
+        float currentX = window.x + window.width * 0.5f;
+
+        Mat roi = image(window); //Extract region of interest
+        vector<Point2f> locations;
+
+        findNonZero(roi, locations); //Get all non-black pixels. All are white in our case
+
+        float avgX = 0.0f;
+
+        for (int i = 0; i < locations.size(); ++i) //Calculate average X position
+        {
+            float x = locations[i].x;
+            avgX += window.x + x;
+        }
+
+        avgX = locations.empty() ? currentX : avgX / locations.size();
+
+        Point point(avgX, window.y + window.height * 0.5f);
+        points.push_back(point);
+
+        //Move the window up
+        window.y -= window.height;
+
+        //For the uppermost position
+        if (window.y < 0)
+        {
+            window.y = 0;
+            shouldBreak = true;
+        }
+
+        //Move x position
+        window.x += (point.x - currentX);
+
+        //Make sure the window doesn't overflow, we get an error if we try to get data outside the matrix
+        if (window.x < 0)
+            window.x = 0;
+        if (window.x + window.width >= imgSize.width)
+            window.x = imgSize.width - window.width - 1;
+
+        if (shouldBreak)
+            break;
     }
-    processedFrame = gaussianBlur;
-    return processedFrame;
+    return points;
 }
 
 int main(int argc, char **argv) {
-
-    string filename = "/Users/pojashad/Desktop/dev/opencvtest/motorcycle.mp4";
+    //---------------GET IMAGE---------------------
+    // Read the image file
+    // TODO: Change to an argument, enter path to image
+    string filename = "/Users/pojashad/Desktop/dev/opencvtest/malibu.mp4";
     VideoCapture cap(filename);
-    //VideoCapture cap(1); // open the video camera no. 0
     if (!cap.isOpened())  // if not success, exit program
     {
         cout << "Cannot open the video cam" << endl;
         return -1;
     }
-    namedWindow("Processed", WINDOW_AUTOSIZE);
+    // ------------------ Birds Eye View of the lane---------------------
+    Point2f srcVertices[4];
+    srcVertices[0] = Point(450, 480);
+    srcVertices[1] = Point(730, 480);
+    srcVertices[2] = Point(1000, 700);
+    srcVertices[3] = Point(200, 700);
+
+    //Destination vertices. Output is 640 by 480px
+    Point2f dstVertices[4];
+    dstVertices[0] = Point(0, 0);
+    dstVertices[1] = Point(640, 0);
+    dstVertices[2] = Point(640, 480);
+    dstVertices[3] = Point(0, 480);
+
+    //Prepare matrix for transform and get the warped image
+    Mat perspectiveMatrix = getPerspectiveTransform(srcVertices, dstVertices);
+    Mat dst(480, 640, CV_8UC3); //Destination for warped image
+
+    //For transforming back into original image space
+    Mat invertedPerspectiveMatrix;
+    invert(perspectiveMatrix, invertedPerspectiveMatrix);
+
+
+    Mat org; //Original image, modified only with result
+    Mat img; //Working image
+
     while (1) {
 
-        Mat src, processedFrame, ROI, processedFrameWithLines, frameBGR;
-        bool bSuccess = cap.read(src); // read a new frame from video
-        if (!bSuccess) {
-            cout << "Cannot read a frame from video stream" << endl;
+        //Read a frame
+        cap.read(org);
+        if (org.empty()) //When this happens we've reached the end
             break;
+
+        //Generate bird's eye view
+        warpPerspective(org, dst, perspectiveMatrix, dst.size(), INTER_LINEAR, BORDER_CONSTANT);
+        //--------------GRAYSCALE IMAGE-----------------
+        // Define grayscale image
+        //Convert to gray
+        cvtColor(dst, img, COLOR_RGB2GRAY);
+
+        // --------------Yellow & White Lines-----------------
+        //Extract yellow and white info
+        Mat maskYellow, maskWhite;
+
+        inRange(img, Scalar(20, 100, 100), Scalar(30, 255, 255), maskYellow);
+        inRange(img, Scalar(150, 150, 150), Scalar(255, 255, 255), maskWhite);
+
+        Mat mask, processed;
+        bitwise_or(maskYellow, maskWhite, mask); //Combine the two masks
+        bitwise_and(img, mask, processed); //Extrect what matches
+
+        // --------------Gaussian Blur-----------------
+        //Blur the image a bit so that gaps are smoother
+        const Size kernelSize = Size(9, 9);
+        GaussianBlur(processed, processed, kernelSize, 0);
+
+        //Try to fill the gaps
+        Mat kernel = Mat::ones(15, 15, CV_8U);
+        dilate(processed, processed, kernel);
+        erode(processed, processed, kernel);
+        morphologyEx(processed, processed, MORPH_CLOSE, kernel);
+
+        //Keep only what's above 150 value, other is then black
+        const int thresholdVal = 175;
+        threshold(processed, processed, thresholdVal, 255, THRESH_BINARY);
+        //Might be optimized with adaptive thresh
+
+        //Get points for left sliding window. Optimize by using a histogram for the starting X value
+        vector<Point2f> pts = slidingWindow(processed, Rect(0, 420, 120, 60));
+        vector<Point> allPts; //Used for the end polygon at the end.
+
+        vector<Point2f> outPts;
+        perspectiveTransform(pts, outPts, invertedPerspectiveMatrix); //Transform points back into original image space
+
+        //Draw the points onto the out image
+        for (int i = 0; i < outPts.size() - 1; ++i)
+        {
+            line(org, outPts[i], outPts[i + 1], Scalar(255, 0, 0), 3);
+            allPts.push_back(Point(outPts[i].x, outPts[i].y));
         }
-        
-        // Ta ut region of interest
-        Rect rect(600, 400, 600, 400);
-        ROI = roi(src, rect);
-        // Process ROI
-       processedFrame = processFrame(ROI);
-        // Convertera tillbaka processerad ROI med linjer till BGR för kunna lägga till i src frame:n
-        frameBGR = convertToBGR(processedFrame);
-        // Merge src filen och den processerade ROI
-        frameBGR.copyTo(src(rect));
-        imshow("Processed", src);
+
+        allPts.push_back(Point(outPts[outPts.size() - 1].x, outPts[outPts.size() - 1].y));
+
+        Mat out;
+        cvtColor(processed, out, COLOR_GRAY2BGR); //Conver the processing image to color so that we can visualise the lines
+
+        for (int i = 0; i < pts.size() - 1; ++i) //Draw a line on the processed image
+            line(out, pts[i], pts[i + 1], Scalar(255, 0, 0));
+
+        //Sliding window for the right side
+        pts = slidingWindow(processed, Rect(520, 420, 100, 40));
+        perspectiveTransform(pts, outPts, invertedPerspectiveMatrix);
+
+        //Draw the other lane and append points
+        for (int i = 0; i < outPts.size() - 1; ++i)
+        {
+            line(org, outPts[i], outPts[i + 1], Scalar(0, 0, 255), 3);
+            allPts.push_back(Point(outPts[outPts.size() - i - 1].x, outPts[outPts.size() - i - 1].y));
+        }
+
+        allPts.push_back(Point(outPts[0].x - (outPts.size() - 1) , outPts[0].y));
+
+        for (int i = 0; i < pts.size() - 1; ++i)
+            line(out, pts[i], pts[i + 1], Scalar(0, 0, 255));
+
+        //Create a green-ish overlay
+        vector<vector<Point>> arr;
+        arr.push_back(allPts);
+        Mat overlay = Mat::zeros(org.size(), org.type());
+        fillPoly(overlay, arr, Scalar(0, 255, 100));
+        addWeighted(org, 1, overlay, 0.5, 0, org); //Overlay it
+
+        //Show results
+        imshow("Preprocess", out);
+        imshow("src", org);
         if (waitKey(30) == 27) {
             cout << "esc key is pressed by user" << endl;
             break;
         }
     }
+    cap.release();
     return 0;
 }
